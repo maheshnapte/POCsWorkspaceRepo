525e5364b1887227c3d6de3532b50dde1b5a6e0b
Mahesh Napte <memaheshnapte@gmail.com>
2018-04-26 10:31:04 +0530

    commit and push after complete code.

--- MockitoGradleStudy/src/test/java/org/mah/FirstMockitoTest.java
+++ MockitoGradleStudy/src/test/java/org/mah/FirstMockitoTest.java
@@ -1,15 +1,44 @@
 package org.mah;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.Test;
 
+//Mocking List
 public class FirstMockitoTest {
 
 	@Test
-	public void test() {
+	public void testListSizeMock() {
 		// fail("Not yet implemented");
-		assertEquals(1, 1);
+		// assertEquals(1, 1);
+		List<String> listMock = mock(List.class);
+		when(listMock.size()).thenReturn(2);
+		assertEquals(2, listMock.size());
 	}
 
+	@Test
+	public void testMultipleListSizeMock() {
+		// fail("Not yet implemented");
+		// assertEquals(1, 1);
+		List<String> listMock = mock(List.class);
+		when(listMock.size()).thenReturn(2).thenReturn(3);
+		assertEquals(2, listMock.size());
+		assertEquals(3, listMock.size());
+	}
+
+	// checking exception
+	@Test(expected=RuntimeException.class)
+	public void testExceptionCheckMock() {
+		// fail("Not yet implemented");
+		// assertEquals(1, 1);
+		List<String> listMock = mock(List.class);
+		when(listMock.get(anyInt())).thenThrow(new RuntimeException());
+		listMock.get(4);
+
+	}
 }
