14b9d060b5dcda2454b24aadfa4efea0f1a1d2ca
Mahesh Napte <memaheshnapte@gmail.com>
2019-02-06 07:55:02 +0530

    initial commit.

--- testOracleConnection/src/com/mah/OracleCon.java
+++ testOracleConnection/src/com/mah/OracleCon.java
@@ -5,7 +5,7 @@ public static void main(String args[]){
 try{  
 //step1 load the driver class  
 Class.forName("oracle.jdbc.driver.OracleDriver");  
-  
+   
 //step2 create  the connection object  
 Connection con=DriverManager.getConnection(  
 "jdbc:oracle:thin:@localhost:1521:xe","system","system");  
