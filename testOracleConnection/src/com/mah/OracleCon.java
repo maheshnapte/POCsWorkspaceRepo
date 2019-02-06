c5fd3b6bbf9b305744458c3a0cfbf8329fa631a6
Mahesh Napte <memaheshnapte@gmail.com>
2019-02-06 07:58:11 +0530

    i

--- testOracleConnection/src/com/mah/OracleCon.java
+++ testOracleConnection/src/com/mah/OracleCon.java
@@ -12,7 +12,7 @@ Connection con=DriverManager.getConnection(
   
 //step3 create the statement object  
 Statement stmt=con.createStatement();  
-  
+   
 //step4 execute query  
 ResultSet rs=stmt.executeQuery("select * from emp");  
 while(rs.next())  
