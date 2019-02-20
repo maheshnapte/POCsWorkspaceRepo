51e6b208fc37a96b85616fe5955db23f25389a3c
Mahesh Napte <memaheshnapte@gmail.com>
2019-02-20 09:34:32 +0530

    Initial commit. Working fine.

---
+++ JdbcTemplateSpring/src/main/java/com/journaldev/spring/dao/PersonDAOImpl.java
@@ -0,0 +1,53 @@
+package com.journaldev.spring.dao;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.journaldev.model.Person;
+import com.journaldev.model.PersonMapper;
+
+@Component
+public class PersonDAOImpl implements PersonDAO {
+
+	JdbcTemplate jdbcTemplate;
+
+	private final String SQL_FIND_PERSON = "select * from people1 where id = ?";
+	private final String SQL_DELETE_PERSON = "delete from people1 where id = ?";
+	private final String SQL_UPDATE_PERSON = "update people1 set first_name = ?, last_name = ?, age  = ? where id = ?";
+	private final String SQL_GET_ALL = "select * from people1";
+	private final String SQL_INSERT_PERSON = "insert into people1(id, first_name, last_name, age) values(?,?,?,?)";
+
+	@Autowired
+	public PersonDAOImpl(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public Person getPersonById(Long id) {
+		return jdbcTemplate.queryForObject(SQL_FIND_PERSON, new Object[] { id }, new PersonMapper());
+	}
+
+	public List<Person> getAllPersons() {
+		List<Person> personList = jdbcTemplate.query(SQL_GET_ALL, new PersonMapper());
+		System.out.println("personList: " + personList);
+		return personList;
+	}
+
+	public boolean deletePerson(Person person) {
+		return jdbcTemplate.update(SQL_DELETE_PERSON, person.getId()) > 0;
+	}
+
+	public boolean updatePerson(Person person) {
+		return jdbcTemplate.update(SQL_UPDATE_PERSON, person.getFirstName(), person.getLastName(), person.getAge(),
+				person.getId()) > 0;
+	}
+
+	public boolean createPerson(Person person) {
+		return jdbcTemplate.update(SQL_INSERT_PERSON, person.getId(), person.getFirstName(), person.getLastName(),
+				person.getAge()) > 0;
+	}
+}
